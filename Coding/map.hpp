674ae248e515574133f6e1b0bee62dfdf1fcec67
Mateusz Kozłowski <trolek222532@wp.pl>
2024-06-04 23:53:01 +0200

    przebudowanie systemu zarządzania obiektami
    zmiana zarządzania z unique_prt na shered_prt co ułatwi zarządzanie oraz modyfikacje wskaźników i pozwoli uniknąć problemów z wyciekiem pamięci

--- Coding/map.hpp
+++ Coding/map.hpp
@@ -11,14 +11,15 @@ class Map : public sf::Drawable {
 private:
     vector<unique_ptr<Sprite>> walls;
     unique_ptr<Sprite> background;
+    Sprite background_sprite;
+    Sprite wall_sprite;
     Texture background_texture;
     Texture wall_texture;
-
-    virtual void draw(RenderTarget& target, RenderStates states) const override;
-
 public:
     Map();
     void create_wall(Vector2f position,Vector2i size);
     void create_background(const Vector2u& windowSize);
     vector<std::unique_ptr<sf::Sprite>>& getWalls();
+    virtual void draw(RenderTarget& target, RenderStates states) const override;
+
 };
